67da2074f8ef17a3389880306e4b2ecac1aba184
Akshay-Durgade <akshayatwwe@gmail.com>
2025-07-26 20:49:43 +0530

    Added vpc

---
+++ modules/vpc/main.tf
@@ -0,0 +1,34 @@
+################################################################################
+# Local Variables
+################################################################################
+locals {
+  create_vpc = var.create_vpc
+}
+
+################################################################################
+# VPC
+################################################################################
+resource "aws_vpc" "main" {
+  count = local.create_vpc ? 1 : 0
+
+  cidr_block       = var.use_ipam_pool ? null : var.cidr_block
+  ipv4_ipam_pool_id   = var.ipv4_ipam_pool_id
+  ipv4_netmask_length = var.ipv4_netmask_length
+
+  assign_generated_ipv6_cidr_block     = var.enable_ipv6 && !var.use_ipam_pool ? true : null
+  ipv6_cidr_block                      = var.ipv6_cidr
+  ipv6_ipam_pool_id                    = var.ipv6_ipam_pool_id
+  ipv6_netmask_length                  = var.ipv6_netmask_length
+  ipv6_cidr_block_network_border_group = var.ipv6_cidr_block_network_border_group
+
+  instance_tenancy                     = var.instance_tenancy
+  enable_dns_hostnames                 = var.enable_dns_hostnames
+  enable_dns_support                   = var.enable_dns_support
+  enable_network_address_usage_metrics = var.enable_network_address_usage_metrics
+
+  tags = merge(
+    { "Name" = var.name },
+    var.tags,
+    var.vpc_tags,
+  )
+}
